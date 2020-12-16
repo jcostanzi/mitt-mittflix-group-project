03b090963cd9e6f53317674b6ff5527198856605
qml <you@email.com>
2020-12-17 01:47:44 +0800

    the function of js now is retriving the pictures when the client input the key words they want to search

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function getImage(src){
+  return new Promise((resolve,reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange',() => {
+      console.log(xhr);
+      if(xhr.readyState === 4 && xhr.status === 200){
+        resolve(JSON.parse(xhr.responseText).Search);
+      }else if (xhr.readystate === 4){
+        reject("the website is blank.");
+      }
+    })
+    xhr.open('GET',src);
+    xhr.send();
+  })
+}
+
+// const myPromise = new Promise((resolve,reject) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.addEventListener('readystatechange',() => {
+//     console.log(xhr);
+//     if(xhr.readyState === 4 && xhr.status === 200){
+//       resolve(JSON.parse(xhr.responseText).Poster);
+//     }else if (xhr.readystate === 4){
+//       reject("the website is blank.");
+//     }
+//   })
+//   xhr.open('GET',"http://www.omdbapi.com/?i=tt3896198&apikey=e12d294b");
+//   xhr.send();
+// })
+
+getImage("http://www.omdbapi.com/?s=galaxy&apikey=e12d294b").then(result => console.log(result), error => console.log(error));
+//getImage("http://www.omdbapi.com/?i=tt3896198&apikey=e12d294b").then(result =>createImage(result), error => console.log(error));
+getImage("http://www.omdbapi.com/?s=galaxy&apikey=e12d294b").then(result =>createImage(result), error => console.log(error));
+
+function createImage(arrayArg){
+  const ul = document.createElement("ul");
+  arrayArg.forEach(element => {
+    const li = document.createElement("li");
+    li.innerHTML = `<img class="fit-picture"
+    src="${element.Poster}"
+    alt="Grapefruit slice atop a pile of other slices">`;
+    ul.appendChild(li);
+  })
+  const body = document.querySelector("body");
+  body.prepend(ul);
+}
