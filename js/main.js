7b8c6a5caaa857d42ec36257afbe47dc0ee1f287
qml <you@email.com>
2020-12-17 08:46:39 +0800

    get the detail information about the movies

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let targetId = [];
+
+
 fetch(`http://www.omdbapi.com/?s=galaxy&apikey=e12d294b`)
   .then(response => {
     if (response.status == 200) {
@@ -7,28 +10,53 @@ fetch(`http://www.omdbapi.com/?s=galaxy&apikey=e12d294b`)
     }
   })
   .then(data => {
+    data.Search.forEach(item => targetId.push(item.imdbID));
+    console.log(targetId);
+    console.log(data);
     createImage(data.Search);
   })
   .catch(error => {
     alert(`Error: "${error}"`);
   });
 
-const movies = document.querySelectorAll(".movie");
+function findcontent(imdbID){
+  let information=[];
+  fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=e12d294b`)
+  .then(response => {
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      Promise.reject(response.statusText);
+    }
+  })
+  .then(data => {
+    information.push(data.Title);
+    information.push(data.Ratings[0].Value);
+    information.push(data.Plot);
+    console.log(information);
+  })
+  .catch(error => {
+    alert(`Error: "${error}"`);
+  });
+}
 
 function createImage(arrayArg) {
-  for (let i = 0; i < 10; i++) {
-    movies[i].firstElementChild.src = arrayArg[i].Poster;
-  }
-  // const ul = document.createElement("ul");
-  // arrayArg.forEach(element => {
-  //   const li = document.createElement("li");
-  //   li.innerHTML = `<img class="fit-picture"
-  //   src="${element.Poster}"
-  //   alt="Grapefruit slice atop a pile of other slices">`;
-  //   ul.appendChild(li);
-  // })
-  // const body = document.querySelector("body");
-  // body.prepend(ul);
+  const container = document.querySelector(".movies-container");
+  arrayArg.forEach(element => {
+    const div = document.createElement("div");
+    div.setAttribute("class","movie");
+    console.log(element);
+
+    findcontent(element.imdbID);
+
+    div.innerHTML = `<img src="${element.Poster}" alt="Poster not available yet">
+    <div class="content">
+      <h1 class="title">title:</h1>
+      <h3 class="rank">rank:</h3>
+      <div class="description">plot:</div>
+    </div>`
+    container.appendChild(div);
+  })
 }
 
 const search = document.querySelector('.search');
