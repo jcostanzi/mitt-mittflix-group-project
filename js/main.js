db83b44ce7c74c2970816fd6214abff7e738e5b1
Javier Costanzi <javier.costanzi@d3sistemas.com.ar>
2020-12-16 19:48:36 -0600

    Final refactor.

--- js/main.js
+++ js/main.js
@@ -1,78 +1,94 @@
-let targetId = [];
-
-
-fetch(`http://www.omdbapi.com/?s=galaxy&apikey=e12d294b`)
-  .then(response => {
-    if (response.status == 200) {
-      return response.json();
-    } else {
-      Promise.reject(response.statusText);
-    }
-  })
-  .then(data => {
-    data.Search.forEach(item => targetId.push(item.imdbID));
-    console.log(targetId);
-    console.log(data);
-    createImage(data.Search);
-  })
-  .catch(error => {
-    alert(`Error: "${error}"`);
-  });
-
-function findcontent(imdbID){
-  let information=[];
-  fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=e12d294b`)
-  .then(response => {
-    if (response.status == 200) {
-      return response.json();
-    } else {
-      Promise.reject(response.statusText);
-    }
-  })
-  .then(data => {
-    information.push(data.Title);
-    information.push(data.Ratings[0].Value);
-    information.push(data.Plot);
-    console.log(information);
-  })
-  .catch(error => {
-    alert(`Error: "${error}"`);
-  });
-}
-
-function createImage(arrayArg) {
-  const container = document.querySelector(".movies-container");
-  arrayArg.forEach(element => {
-    const div = document.createElement("div");
-    div.setAttribute("class","movie");
-    console.log(element);
-
-    findcontent(element.imdbID);
-
-    div.innerHTML = `<img src="${element.Poster}" alt="Poster not available yet">
-    <div class="content">
-      <h1 class="title">title:</h1>
-      <h3 class="rank">rank:</h3>
-      <div class="description">plot:</div>
-    </div>`
-    container.appendChild(div);
-  })
-}
-
-const search = document.querySelector('.search');
+const searchElem = document.querySelector('.search');
 const deleteBtn = document.querySelector('.delete');
+const container = document.querySelector(".movies-container");
+
+let movies = [];
+
+function search(keyword) {
+  container.innerHTML = '';
+  movies = [];
+  fetch(`http://www.omdbapi.com/?s=${keyword}&apikey=e12d294b`)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        Promise.reject(response.statusText);
+      }
+    })
+    .then(data => {
+      if (data.Response == 'False') {
+        Promise.reject(data.Error);
+        throw data.Error;
+      }
+      return data;
+    })
+    .then(data => {
+      data.Search.forEach(e => {
+        const movie = {
+          imdbID: e.imdbID,
+          Poster: e.Poster,
+        };
+        if (movie.Poster == 'N/A') {
+          movie.Poster = 'img/poster-placeholder.png';
+        }
+        movies.push(movie);
+      });
+
+      movies.forEach(m => {
+        findContent(m);
+      });
+    })
+    .catch(error => {
+      alert(`Error: "${error}"`);
+    });
+}
+
+function findContent(movie) {
+  return fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=e12d294b`)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        Promise.reject(response.statusText);
+      }
+    })
+    .then(data => {
+      movie.Title = data.Title;
+      movie.Rating = data.Ratings[0].Value;
+      movie.Plot = data.Plot;
+
+      createImage(movie);
+    })
+    .catch(error => {
+      alert(`Error: "${error}"`);
+    });
+}
+
+function createImage(movie) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "movie");
+
+  div.innerHTML = `<img src="${movie.Poster}" alt="Poster not available yet">
+  <div class="content">
+    <h1 class="title">${movie.Title}</h1>
+    <h3 class="rank">${movie.Rating}</h3>
+    <div class="description">${movie.Plot}</div>
+  </div>`
+  container.appendChild(div);
+}
 
 deleteBtn.addEventListener('click', function(event) {
   console.log("deletebtn");
-  search.value = '';
+  searchElem.value = '';
 })
 
-search.addEventListener('keypress', function (e) {
+searchElem.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
     e.preventDefault();
-    if (search.value !== '') {
-      search.innerHTML = search.value;
-      search.value = '';
+    if (searchElem.value !== '') {
+      search(searchElem.value);
     }
   }
 });
+
+search('galaxy');
